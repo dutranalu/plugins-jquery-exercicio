f24036526ad462cdb633b1ede48f6ae73554fbaf
analu <dutranalu@hotmail.com>
2025-11-12 15:41:48 -0300

    correção dos arquivos

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    // === CAROUSEL SLICK ===
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        dots: true,
+        arrows: false
+    });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    // === MÁSCARAS ===
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '01234-567'
+    });
+
+    // === VALIDAÇÃO ===
+    $('#form').validate({
+        rules: {
+            nome: { required: true },
+            email: { required: true, email: true },
+            telefone: { required: true },
+            endereco: { required: true },
+            cep: { required: true },
+            cpf: { required: true }
         },
-        email: {
-            requerido: true,
-            email: true
+        messages: {
+            nome: 'Por favor, insira seu nome completo',
+            email: 'Insira um e-mail válido',
+            telefone: 'Informe seu telefone',
+            endereco: 'Informe seu endereço completo',
+            cep: 'Informe seu CEP',
+            cpf: 'Informe seu CPF'
         },
-        telefone: {
-            requerido: true
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha todos os campos para prosseguir com a compra!");
+        }
+    });
+});
